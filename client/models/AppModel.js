b8b9e017bff16ce2775056c12149b5de4130ac0c
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-02 14:27:05 -0700

    Test commit

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,6 +1,8 @@
 // App.js - Defines a backbone model class for the whole app.
 var AppModel = Backbone.Model.extend({
 
+  
+
   initialize: function(params) {
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
